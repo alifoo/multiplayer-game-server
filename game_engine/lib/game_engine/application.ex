b98806afeeec3d883ff1be52f75bb55f7f998068
alifoo <alissonayres.trb@gmail.com>
2026-04-11 21:15:25 -0300

    application default world map

--- game_engine/lib/game_engine/application.ex
+++ game_engine/lib/game_engine/application.ex
@@ -12,10 +12,33 @@ defmodule GameEngine.Application do
       # {GameEngine.Worker, arg}
       {Registry, keys: :unique, name: GameEngine.Registry},
       GameEngine.WorldSupervisor,
-      GameEngine.PlayerSupervisor
+      GameEngine.PlayerSupervisor,
+      %{
+        id: :zone_bootstrapper,
+        start: {Task, :start_link, [fn -> boot_initial_zones() end]},
+        restart: :temporary
+      }
     ]
 
     opts = [strategy: :one_for_one, name: GameEngine.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp boot_initial_zones do
+    IO.puts("Bootstrapping initial zones...")
+
+    world_map = %{
+      "zone_1" => %{name: "Green Fields", description: "A peaceful grassy area."},
+      "zone_2" => %{name: "Dark Forest", description: "A spooky, dense forest."},
+      "zone_3" => %{name: "Crystal Caves", description: "Shiny caves filled with crystals."},
+      "zone_4" => %{name: "Volcanic Wasteland", description: "A harsh, fiery landscape."},
+      "zone_5" => %{name: "Sky Islands", description: "Floating islands in the sky."}
+    }
+
+    Enum.each(world_map, fn {zone_id, _info} ->
+      GameEngine.WorldSupervisor.start_zone(zone_id)
+    end)
+
+    GameEngine.WorldSupervisor.start_zone(:zone_1)
   end
 end
